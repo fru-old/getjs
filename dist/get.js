ca6d60be850b2bcf14e429296a09dc199aa8bd2a
Florian Rüberg <florian.rueberg@gmail.com>
2014-08-13 22:54:19 +0200

    added query support

--- dist/get.js
+++ dist/get.js
@@ -685,54 +685,129 @@ function Query(){
 	this.traces = [];
 }
 
-Query.buildStateMachine = function(traces){
-
-	function equals(target, value){
-		return target === value;
+function buildAssertion(type, name, operator, valueName, data){
+	var predicate;
+	switch (operator) {
+		case 'in':
+			predicate = function(a,b){ return a.indexOf(b) !== -1; };
+			break;
+		case '==':
+		case '===':
+			predicate = function(a,b){ return a == b; };
+			break;
+		case '!=':
+		case '!==':
+			predicate = function(a,b){ return a != b; };
+			break;
+		case '<':
+			predicate = function(a,b){ return a < +b; };
+			break;
+		case '<=':
+			predicate = function(a,b){ return a <= +b; };
+			break;
+		case '>':
+			predicate = function(a,b){ return a > +b; };
+			break;
+		case '>=':
+			predicate = function(a,b){ return a >= +b; };
+			break;
+		case null:
+			predicate = function(a,b){ return a !== undefined; };
+			break;
+		default:
+			throw new Error('Unknown operator '+operator);
 	}
 
-	// TODO: Can this not go into the parser?
-	function buildAssertions(assertions, result){
-		var truthy = [];
-		assertions.map(function(assertion){
+	var value;
+	if(valueName.length === 1 && valueName[0].breakets){
+		value = data(valueName[0].breakets);
+	}else if(!operator){
+		value = null;
+	}else{
+		value = readName(valueName, data);
+	}
+	return new machine.Assertion(type, name, predicate, value);
+}
+
+function readName(name, data){
+	var result = '';
+	for(var i in name){
+		if(name[i].breakets){
+			result += data(name[i].breakets);
+		}else if(name[i].constant){
+			result += name[i].constant;
+		}else if(name[i].wildcard){
+			throw new Error('* not supported here.');
+		}
+	}
+	return result;
+}
+
+function isWildcard(check, count){
+	if((check[0]||{}).type === '_'){
+		// {type: '_', name: [{wildcard: 2}]}
+		var number = (check[0].name||{}).wildcard;
+		return number === count;
+	}
+	return false;
+}
+
+function buildAssertions(assertions, data){
+	var truthy = [];
+	assertions.map(function(assertion){
+		if(isWildcard(assertion, 1)){
+			truthy.push(machine.Assertion.truthy);
+		}else{
+			var type, name, operator = null, value;
 			switch (assertion.type) {
 				case '_':
-					if(assertion.name.length === 1 && assertion.name[0].constant){
-						truthy.push(new machine.Assertion(
-							'tags', 'name', equals, assertion.name[0].constant
-						));
-					}else if(assertion.name.wildcard===1){
-						truthy.push(machine.Assertion.truthy);
-					}else{
-						// TODO
-					}
+					type = 'tags';
+					name = 'name';
+					operator = '==';
+					value = assertion.name;
 					break;
-				// TODO
 				case '#':
+					type = 'attr';
+					name = 'id';
+					operator = '==';
+					value = assertion.name;
 					break;
 				case '[':
+					type = 'prop';
+					name = assertion.prop;
+					if(assertion.assert){
+						operator = assertion.assert;
+						value = assertion.value;
+					}
 					break;
 				case ':':
+					if(assertion.prop){
+						type = assertion.name;
+						name = assertion.prop;
+						if(assertion.assert){
+							operator = assertion.assert;
+							value = assertion.value;
+						}	
+					}else{
+						type = 'attr';
+						name = 'class';
+						operator = 'in';
+						value = assertion.name;
+					}
 					break;
 			}
-		});
-		return new machine.DNF(truthy);
-	}
-
-	function isWildcard(state, count){
-		if(state.length === 1 && (state[0]||{}).type === '_'){
-			// {type: '_', name: [{wildcard: 2}]}
-			var number = (state[0].name||{}).wildcard;
-			return number === count;
+			truthy.push(buildAssertion(type, name, operator, value));
 		}
-		return false;
-	}
+	});
+	return new machine.DNF(truthy);
+}
 
+Query.buildStateMachine = function(traces, data){
 	var result;
 	traces.map(function(trace){
 		var states = new machine.States();
 		trace.map(function(state, index){
-			if(isWildcard(state,2)){
+			if(state.length === 1 && isWildcard(state,2)){
 				var truthy = machine.Assertion.truthy;
 				states.addTransition(index, index+1, truthy);
 				states.addTransition(index, index, truthy);
@@ -748,10 +823,6 @@ Query.buildStateMachine = function(traces){
 			result = new machine.DNF([states]);
 		}
 	});
-
-	//if(!result){
-		// TODO
-	//}
 	return result;
 };
 
